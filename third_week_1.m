71b208145396b4aeb932c778b6ec22c7d3aeb72e
XuanjieXiao <872272705@qq.com>
2021-04-03 20:40:23 +0800

    2021-04-03-01

--- third_week_1.m
+++ third_week_1.m
@@ -1,3 +1,9 @@
-clc;
-clear all;
-close all;
+%% 代码信息
+%author：JamesRemington
+%E-mail:xuanjiexiao@163.com
+%date:2021-04-3
+%copyright:2020-2021
+%version 
+clc;%清屏
+clear all;%清除数据内存
+close all;%清楚所有图片
